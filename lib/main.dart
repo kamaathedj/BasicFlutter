f338eaea5cb4fd447f408ee3c51089e6d3323aa5
kamaathedj <kamaudavikiruku@gmail.com>
2024-02-10 11:32:46 +0300

    :sparkles: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('my app')),
+        body: Center(
+          child: ListView.builder(itemBuilder: (context, index) {
+            return ListTile(
+              title: Text('title'),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
